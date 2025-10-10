13d46bd132ac371dfb7630174f1d6714871085dc
Darknab <darknab@live.fr>
2025-10-10 19:20:25 +0100

    Add getConfig and a helper function that tests the outcome of getConfig.

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,22 @@
 export function sayHello() {
     return 'Hello!'
 }
+
+export function getConfig() {
+    if (typeof __YALIDINE_CONFIG__ === undefined || !__YALIDINE_CONFIG__) {
+        throw new Error(
+            "astro-yalidine: Configuration missing. Did you add yalidine to astro.config.mjs ?"
+        )
+    }
+    return __YALIDINE_CONFIG__;
+}
+
+export function checkConfig() {
+    const { apiKey, apiUrl } = getConfig()
+
+    if (!apiUrl) {
+        console.log('something went wrong!')
+    } else {
+        console.log(apiUrl)
+    }
+}
