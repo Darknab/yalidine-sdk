96a92f1113b8034accc4caa2ded580cf25644432
Darknab <darknab@live.fr>
2025-12-26 22:31:52 +0100

    Fix typo.

--- packages/yalidine-sdk/src/helpers/index.js
+++ packages/yalidine-sdk/src/helpers/index.js
@@ -1,5 +1,5 @@
 export * from './calculateBillabeWeight.js';
-export * from './calculateParcels.js';
+export * from './createParcels.js';
 export * from './getCentersByCommune.js';
 export * from './getCentersByWilaya.js';
 export * from './getCommunesByWilaya.js';
